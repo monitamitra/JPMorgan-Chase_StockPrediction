0870aa7f22cdd10619b15109b5bd94c9bc0aedb4
Monita <mitramonita@gmail.com>
2023-12-16 12:23:26 -0600

    changed streamlit interface

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ st.title("Predicting JP Morgan & Chase Co. Stock Trends")
 st.markdown("LSTM model aims to forecast the monthly stock direction of JP Morgan Chase & Co. \
         when provided with a date as its input")
 
-col1, col2 = st.columns(2)
+col1 = st.columns(1)
+
+with st.sidebar:
+        date = st.date_input("Choose a date to predict stock trends")
 
 with col1:
-        user_input = st.text_input("Enter a date (YYYY-MM-DD) to predict stock trends")
-
-with col2:
         st.text("Prediction")
         button_predict = st.button("Predict")
 
-if button_predict and user_input:
-        result = predict_stock_trend(user_input)
+if button_predict and date:
+        result = predict_stock_trend(date)
         st.pyplot(fig=result, clear_figure=True)
