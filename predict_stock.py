900dc5c0b8651706669bf13b541e69c58c8fbef0
Monita <mitramonita@gmail.com>
2023-12-16 13:20:17 -0600

    changed location of legend

--- predict_stock.py
+++ predict_stock.py
@@ -71,7 +71,7 @@ def predict_stock_trend(input_date):
     prediction_points = mlines.Line2D([], [], color = "black", marker = "o", markerfacecolor="None", linestyle='None', 
         label = "Daily Closing Points")
     
-    plt.legend(handles = [prediction_points], fontsize = 10, loc = "upper left")
+    plt.legend(handles = [prediction_points], fontsize = 9, loc = "upper right")
 
     plt.tight_layout()
 
