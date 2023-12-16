b58ddd0e8cc07955f4560fb7b240fcf51f66e1d6
Monita <mitramonita@gmail.com>
2023-12-16 12:24:43 -0600

    removed column

--- app.py
+++ app.py
@@ -6,14 +6,12 @@ st.title("Predicting JP Morgan & Chase Co. Stock Trends")
 st.markdown("LSTM model aims to forecast the monthly stock direction of JP Morgan Chase & Co. \
         when provided with a date as its input")
 
-col1 = st.columns(1)
-
 with st.sidebar:
         date = st.date_input("Choose a date to predict stock trends")
 
-with col1:
-        st.text("Prediction")
-        button_predict = st.button("Predict")
+
+st.text("Prediction")
+button_predict = st.button("Predict")
 
 if button_predict and date:
         result = predict_stock_trend(date)
