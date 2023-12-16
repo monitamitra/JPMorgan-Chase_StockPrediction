5b7d92ca26a7ec17b3535c837954fb8f18b53c13
Monita <mitramonita@gmail.com>
2023-12-16 10:22:15 -0600

    removed widget error

--- app.py
+++ app.py
@@ -6,17 +6,15 @@ st.title("Predicting JP Morgan & Chase Co. Stock Trends")
 st.markdown("LSTM model aims to forecast the monthly stock direction of JP Morgan Chase & Co. \
         when provided with a date as its input")
 
-st.header("Date")
 col1, col2 = st.columns(2)
 
 with col1:
-        st.text_input("Enter a date (YYYY-MM-DD) to predict stock trends")
+        user_input = st.text_input("Enter a date (YYYY-MM-DD) to predict stock trends")
 
 with col2:
         st.text("Prediction")
-        
-        
-        st.button("Predict")
-        if st.button("Predict") and st.text_input("Enter a date (YYYY-MM-DD) to predict stock trends"):
-                        result = predict_stock_trend(text_input)
-                        st.pyplot(fig=result, clear_figure=True)
+        button_predict = st.button("Predict")
+
+if button_predict and user_input:
+        result = predict_stock_trend(user_input)
+        st.pyplot(fig=result, clear_figure=True)
