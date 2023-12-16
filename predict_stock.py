e60a8a29f0f6c9080d0d47f321077c80a6beed71
Monita <mitramonita@gmail.com>
2023-12-16 16:43:36 -0600

    m

--- predict_stock.py
+++ predict_stock.py
@@ -71,7 +71,6 @@ def predict_stock_trend(input_date):
     
     prediction_points = mlines.Line2D([], [], color = "black", marker = "o", markerfacecolor="None", linestyle='None', 
         label = "Daily Closing Points")
-    plt.xticks(predictions.flatten())
     
     plt.legend(handles = [prediction_points], fontsize = 9, loc = "upper left")
 
